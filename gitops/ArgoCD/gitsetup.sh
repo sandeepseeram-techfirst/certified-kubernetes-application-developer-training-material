de940f4aef4cfafa93bb884756574d47299651af
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-09-01 18:58:07 +0530

    CKAD Certification Prep

--- gitops/ArgoCD/gitsetup.sh
+++ gitops/ArgoCD/gitsetup.sh
@@ -2,12 +2,12 @@ cd /usercode/system
 mv conf .gitignore 
 export GH_TOKEN={{GITHUB_PAT}}
 git config --global credential.helper store 
-git config --global user.email "seerams@acm.org"
+git config --global user.email "seerams@acm.org" 
 git config --global user.name "ACM Technologies" 
 echo "https://{{GITHUB_USERNAME}}:{{GITHUB_PAT}}@github.com" >> ~/.git-credentials
 git init
 git add .gitignore 
-git commit -m "Lesson started" 
+git commit -m "Lesson started"  
 git add .
 git commit -m "Remaining lesson files"
 git branch -m main

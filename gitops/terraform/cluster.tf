57dd00c7b0187a1bb1ffbef0e69e2b7c7f133917
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-09-05 21:19:26 +0530

    CKAD Certification Prep

--- gitops/terraform/cluster.tf
+++ gitops/terraform/cluster.tf
@@ -21,7 +21,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     name            = "default" 
     node_count      = 2
     vm_size         = "Standard_D2_v2"
-    os_disk_size_gb = 30 
+    os_disk_size_gb = 30
   }
 
   service_principal {

6aad080c05670c97f6e5475c5ed5afedf5e39151
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-09-16 18:18:53 +0530

    terraform ec2 instance

--- ami/ec2instance.tf
+++ ami/ec2instance.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "example" {
               #!/bin/bash 
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p 8080 &
-              EOF
+              EOF 
 
   user_data_replace_on_change = true 
 

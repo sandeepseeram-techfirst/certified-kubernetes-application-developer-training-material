c4e0e11583f24975df4b7c8007e0229c56a600a0
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-09-20 18:28:50 +0530

    terraform

--- ami/securitygroup.tf
+++ ami/securitygroup.tf
@@ -3,7 +3,7 @@ resource "aws_security_group" "instance" {
 
   ingress {
     from_port   = 8080  
-    to_port     = 8080
+    to_port     = 8080 
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }

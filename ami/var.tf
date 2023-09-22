f2adaf01deb32377a48f26d2d5170c99c53a130f
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-09-22 18:31:43 +0530

    terraform

---
+++ ami/var.tf
@@ -0,0 +1,16 @@
+variable "object_example" {
+  description = "An example of a structural type in Terraform"
+  type        = object({
+    name    = string
+    age     = number
+    tags    = list(string)
+    enabled = bool
+  })
+
+  default = {
+    name    = "value1"
+    age     = 42
+    tags    = ["a", "b", "c"]
+    enabled = true
+  }
+}

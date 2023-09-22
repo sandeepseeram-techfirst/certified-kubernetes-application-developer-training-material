4b974ed2dff117cf7559ce9142b842c6204df5a6
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-09-22 18:31:24 +0530

    terraform

--- ami/server.tf
+++ ami/server.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "app" {
-  instance_type     = "t2.micro"
+  instance_type     = "t2.micro" 
   availability_zone = "us-east-2a"
   ami               = "ami-0fb653ca2d3203ac1"
 

02fa1f57dc1513cef06e759b2a983e28cbfcc062
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-09-26 18:49:18 +0530

    terraform

---
+++ ami/launch_configuration.tf
@@ -0,0 +1,11 @@
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0fb653ca2d3203ac1"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id] 
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+}

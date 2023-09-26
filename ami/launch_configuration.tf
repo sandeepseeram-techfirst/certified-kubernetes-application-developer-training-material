9c40f48c2a4b32c3a568d8a0cb4ad4bd1015497b
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-09-26 18:53:20 +0530

    terraform

--- ami/launch_configuration.tf
+++ ami/launch_configuration.tf
@@ -1,7 +1,7 @@
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-0fb653ca2d3203ac1"
   instance_type   = "t2.micro"
-  security_groups = [aws_security_group.instance.id] 
+  security_groups = [aws_security_group.instance.id]  
 
   user_data = <<-EOF
               #!/bin/bash

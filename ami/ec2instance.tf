77570549703fba2e348812729f850b8d664899e8
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-09-29 19:02:59 +0530

    terraform

--- ami/ec2instance.tf
+++ ami/ec2instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "example" {
   ami                    = "ami-0fb653ca2d3203ac1"
   instance_type          = "t2.micro"  
-
+ 
   user_data = <<-EOF
               #!/bin/bash  
               echo "Hello, World" > index.html 
